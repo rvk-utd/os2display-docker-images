9e4ba166cac6aaa6df5d67d6d2d28f73757b8e93
Mads H. Danquah <mads@danquah.dk>
2019-06-03 12:13:51 +0200

    Switch to php 7.2 to support os2display-admin 6.x

--- images/admin-php/files/bin/run_os2display_cron.sh
+++ images/admin-php/files/bin/run_os2display_cron.sh
@@ -12,5 +12,16 @@ fi
 
 cd /var/www/admin
 echo "running in $APP_ENV"
+
+# The location of console changes between admin-releases - support both.
+if [[ -x app/console ]]; then
+  CONSOLE="app/console"
+elif [[ -x bin/console ]]; then
+  CONSOLE="bin/console"
+else
+  echo "Could not find console"
+  exit 1
+fi
+
 # We pipe stderr to stdout as jobber won't log stderr.
-APP_ENV=${APP_ENV} gosu www-data app/console os2display:core:cron 2>&1
+APP_ENV=${APP_ENV} gosu www-data $CONSOLE os2display:core:cron 2>&1
